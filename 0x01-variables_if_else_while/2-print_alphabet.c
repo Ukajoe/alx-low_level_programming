bde839da455a85eadf9898e8466d1fe187d0d7cc
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-09 06:28:50 -0700

    Letter game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char letter;
+
+
+for (letter = a; letter <= z; letter++)
+putchar(letter);
+
+
+putchar(n);
+
+return (0);
+}
