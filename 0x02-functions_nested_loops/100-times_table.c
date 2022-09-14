8411e61b1391c8f8298984ddaf5053255452fcb8
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-13 23:15:26 -0700

    multiply

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,34 +24,34 @@ z = x * y;
 if (z > 99)
 {
 
-_putchar(,);
+_putchar(',');
 _putchar(32);
-_putchar((z / 100) + 0);
-_putchar(((z / 10) % 10) + 0);
-_putchar((z % 10) + 0);
+_putchar((z / 100) + '0');
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
 }
 else if (z > 9)
 {
-_putchar(,);
+_putchar(',');
 _putchar(32);
 _putchar(32);
-_putchar(((z / 10) % 10) + 0);
-_putchar((z % 10) + 0);
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
 }
 else
 {
 if (y != 0)
 {
-_putchar(,);
+_putchar(',');
 _putchar(32);
 _putchar(32);
 _putchar(32);
 
 }
-_putchar(z + 0);
+_putchar(z + '0');
 }
 }
-_putchar(n);
+_putchar('\n');
 }
 }
 }
