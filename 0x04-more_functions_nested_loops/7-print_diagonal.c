bdd12f8783cdc0efbdf8d70b438d3b20f6925048
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-15 23:02:11 -0700

    Number

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,5 @@
 
-#include "main.h"
+#include main.h
 #include <studio.h>
 #include <stdlib.h>
 /**
@@ -15,15 +15,15 @@ void print_diagonal(int n)
 int i, j;
 
 if (n <= 0)
-_putchar('\n');
+_putchar(n);
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
 {
-_putchar(' ');
+_putchar( );
 }
-_putchar('\');
-_putchar('\n');
+_putchar(\);
+_putchar(n);
 }
 
 }
