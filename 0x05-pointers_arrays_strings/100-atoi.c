75abddcab0937490197ab6fb258504c7c4c14623
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-20 12:17:40 -0700

    Number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,5 +24,5 @@ num = (num * 10) + (*s - '0');
 else if (num > 0)
 break;
 } while (*s++);
-return ((num * sign));
+return (num 0-reset_to_98.c 1-swap.c 2-strlen.c 3-puts.c 4-print_rev.c 5-rev_string.c 6-puts2.c 7-puts_half.c 8-print_array.c 9-strcpy.c main.h README.md sign);
 }
