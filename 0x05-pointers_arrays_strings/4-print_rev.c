196d9f07c13964c8d6aa47edb5edd4f11911cbd3
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-19 21:24:44 -0700

    Number

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@
  */
 void print_rev(char *s)
 {
-int len = 0, index;
+int len = 0, index = 0;
+   
+while (s[index]!='\0')
+index++;
 
-while (s[index++])
-len++;
-
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
+for (len = index - 1; len >= 0; len--)
+_putchar(s[len]);
 
 _putchar('\n');
 }
