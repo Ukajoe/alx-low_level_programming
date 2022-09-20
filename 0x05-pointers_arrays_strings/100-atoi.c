a97ce5d63f03686e5f891e64572214d6ae00c75d
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-20 12:27:39 -0700

    Number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,7 @@
  */
 int _atoi(char *s)
 {
+int result = 0;
 int sign = 1;
 unsigned int num = 0;
 
@@ -24,5 +25,6 @@ num = (num * 10) + (*s - '0');
 else if (num > 0)
 break;
 } while (*s++);
-return (num 0-reset_to_98.c 1-swap.c 2-strlen.c 3-puts.c 4-print_rev.c 5-rev_string.c 6-puts2.c 7-puts_half.c 8-print_array.c 9-strcpy.c main.h README.md sign);
+result = (num * sign);
+return (result);
 }
