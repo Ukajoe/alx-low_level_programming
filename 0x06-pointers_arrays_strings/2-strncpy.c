0db86234b77217a5af1fd6d5b80eca6ad029d0be
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-21 10:32:12 -0700

    Number

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * _strncpy - Copies at most an inputted number of bytes from string src into dest.
+ * _strncpy - Copies at most an inputted number of bytes
+ * from string src into dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
  * @n: The maximum number of bytes to copied from src.
