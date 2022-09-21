57270edbdf0f131fb78b6e725dd8bec63bbbe583
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-09-21 13:32:30 -0700

    Number

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,12 @@
 /**
  * _strcmp - Compares strings.
  * @s1: A pointer to compared.
- * @s2: A pointer to the second 
+ * @s2: A pointer to the second
  * Return: If str1 < str2
  * of the first unmatched characters.
  * If str1 == str2, 0.
  * If str1 > str2, the positive
- * difference of the first 
+ * difference of the first
  */
 int _strcmp(char *s1, char *s2)
 {
