d5d5f5dbd57dd6b28fab3b80eb561a76997d06ab
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2022-10-27 14:47:00 -0700

    Number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,7 +9,6 @@
  * written in the 0x14-bit_manipulation directory.
  */
 
-int _putchar(int c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -17,6 +16,8 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+int _putchar(char c);
+
 
 #endif /* MAIN_H */
 
